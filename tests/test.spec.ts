951f1af13ae92becb471a7e7e33b919b04b957f9
Artem Oganesyan <artem.oganesyan@gmail.com>
2026-02-26 19:31:41 -0800

    init

---
+++ tests/test.spec.ts
@@ -0,0 +1,10 @@
+console.log('Loading test.spec.ts...');
+import { test, expect } from '@playwright/test';
+import { config } from '../config';
+
+test.describe('Quote form tests', () => {
+
+  test('should fill out and verify required fields on quote form', async ({ page }) => {
+
+  });
+});
