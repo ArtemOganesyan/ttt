78f9eb59a8969322918b61a5308381dc078b843b
Alina Syniagina <alina.sinyagina@gmail.com>
2026-03-09 17:41:41 -0700

    added tests USPS

--- tests/alina.spec.ts
+++ tests/alina.spec.ts
@@ -1,3 +1,24 @@
-//Test1
-//another change
-//change 3
+import { test, expect } from '@playwright/test';
+
+test.describe('USPS Website Tests', () => {
+
+  test('Verify homepage loads', async ({ page }) => {
+    await page.goto('https://www.usps.com/');
+    await expect(page).toHaveTitle(/USPS/i);
+    const logo = page.locator('.global-logo');
+    await expect(logo).toBeVisible();
+  });
+
+
+  test('Track a package with invalid tracking number', async ({ page }) => {
+    await page.goto('https://www.usps.com/');
+    await page.fill('#home-input', '9205500000000000000000');
+    await page.click('.quicktools-featured button[value="Search"]');
+    await expect(page.locator('.container-fluid.full-subheader')).toContainText('USPS Tracking');
+    await expect(page.locator('.tracking-label')).toContainText('Tracking Number');
+    await expect(page.locator('.tracking-number')).toHaveText('9205500000000000000000');
+    await expect(page.locator('h3.banner-header')).toHaveText('Tracking Not Available');
+  });
+
+
+});
